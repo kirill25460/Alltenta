4f9dd099586675904b2a9fae49f76c582dfae6a9
Ponedilok Danyyl <danya253545@gmail.com>
2024-03-04 21:09:20 +0200

    slider

--- src/components/Gallery/Gallery.styled.js
+++ src/components/Gallery/Gallery.styled.js
@@ -1,7 +1,21 @@
 import styled from 'styled-components';
+import { IoIosArrowRoundBack } from 'react-icons/io';
+import { IoIosArrowRoundForward } from 'react-icons/io';
 
 export const GalleryConteiner = styled.div`
   text-align: center;
+`;
+
+export const ButtonConteiner = styled.div`
+  display: flex;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-top: 30px;
+`;
+
+export const ButtonBlockItem = styled.div`
+  margin-right: 110px;
 `;
 
 export const MainGalleryText = styled.h1`
@@ -25,4 +39,26 @@ export const GalleryText = styled.p`
   width: 613px;
   display: inline-block;
   color: #767676;
+`;
+
+export const GalleryBlockConteiner = styled.div``;
+
+export const GalleryItemImage = styled.img`
+  width: 360px;
+  height: 360px;
+  transition: filter 0.3s ease;
+  cursor: pointer;
+
+  &:hover {
+    filter: brightness(0.6);
+  }
+`;
+
+export const ArrowLeft = styled(IoIosArrowRoundBack)`
+  width: 45px;
+  height: 40px;
+`;
+export const ArrowRight = styled(IoIosArrowRoundForward)`
+  width: 45px;
+  height: 40px;
 `;
